356b7efac850f92f5502cd354fa2e99f9b90da47
Alex Newlifer <alex.newlifer@gmail.com>
2024-09-25 23:38:38 +0300

    Actix test =)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{dev::AppService, web, App, Error, HttpResponse, HttpServer};
+use dotenvy::dotenv;
+
+pub async fn test() -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().json("I'm teapot"))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let server = HttpServer::new(move || {
+        App::new().service(
+            web::resource("/users")
+                .route(web::get().to(test)),
+        )
+    })
+    .bind("127.0.0.1:8080")?
+    .run();
+    println!("Server running!");
+
+    server.await
 }
